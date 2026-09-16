680bd2338512cb44b9a0cdc1a857787e0cee2124
Vivek prasad <prasadvivek343@gmail.com>
2026-09-17 00:34:53 +0530

    01_basic_operations.py

---
+++ Assignment1/01_basic_operations.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)
+print("Division =", a / b)
